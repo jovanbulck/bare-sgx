c02811acc34f53b5aa216653052d991e0399cf23
Daan Vanoverloop <daan@vanoverloop.xyz>
2026-05-22 09:59:57 +0000

    Fix sgx_get_tcs when using sgxs enclaves

--- urts/sgxs.c
+++ urts/sgxs.c
@@ -27,6 +27,7 @@ int g_fd_dev = 0, g_ecreated = 0;
 uint64_t g_encl_base = 0;
 uint64_t g_encl_size = 0;
 uint64_t g_encl_tcs = 0;
+extern void *baresgx_tcs;
 
 static uint64_t map_enclave_area(uint64_t encl_size)
 {
@@ -206,5 +207,6 @@ void* baresgx_load_sgxs_enclave(const char *sgxs_path, const char * sigstruct_pa
 	close(fd_sig);
 	close(g_fd_dev);
 	
+	baresgx_tcs = (void*) g_encl_tcs;
 	return (void*) g_encl_tcs;
 }
